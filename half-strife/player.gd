29676e6a9b9f20a415f4228689950dd084b57ebc
CM3030-plays <plays.cm3030@gmail.com>
2026-04-21 11:33:19 -0600

    tweak player animations

--- half-strife/player.gd
+++ half-strife/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var SPEED = 500.0
 @export var ACCELERATION = 10.0
 
+const playerWalkAngle = [45, -45]
+
 var currentWeapon
 var animation
 
@@ -22,12 +24,30 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _process(delta: float) -> void:
-	if (Input.is_action_pressed("click")):
+	if Input.is_action_pressed("click"):
 		animation = "Attack"
-		$AttackCooldown.start()
+		if $AttackCooldown.is_stopped():
+			$AttackCooldown.start()
 	else:
-		if $AttackCooldown.timeout:
-			animation = "Idle"
+		if $AttackCooldown.is_stopped():
+			if velocity.is_zero_approx():
+				animation = "Idle"
+			else:
+				var forward = Vector2.RIGHT.rotated(rotation)
+				var move_dir = velocity.normalized()
+				
+				var angle = forward.angle_to(move_dir) # radians
+				
+				if abs(angle) <= deg_to_rad(45):
+					animation = "Walk"
+				else:
+					animation = "Idle"
+
+	$PlayerSprite.animation = str(currentWeapon) + animation
 		
 func attack():
 	pass
+
+
+func _on_attack_cooldown_timeout() -> void:
+	$AttackCooldown.stop()
